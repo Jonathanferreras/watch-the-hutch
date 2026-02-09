1bfc3d3416fda9314bc0ac66860e48e0434941a1
jonathanferreras <jonathanferreras@icloud.com>
2026-02-09 11:15:18 -0500

    moved static folder in app folder and renamed to client

--- app/server.py
+++ app/server.py
@@ -14,6 +14,8 @@ logger.setLevel(logging.DEBUG)
 
 v1_prefix = "/api/v1"
 app = FastAPI()
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+CLIENT_DIR = os.path.join(BASE_DIR, "client")
 
 @app.on_event("startup")
 def on_startup():
@@ -25,12 +27,12 @@ app.include_router(events_controller.router, prefix=v1_prefix)
 app.include_router(state_controller.router, prefix=v1_prefix)
 app.include_router(admin_controller.router, prefix=f"{v1_prefix}/admin")
 app.include_router(webrtc_controller.router)
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/static", StaticFiles(directory=CLIENT_DIR), name="static")
 
 @app.get("/", response_class=FileResponse)
 async def serve_home():
-    return FileResponse(os.path.join("static", "index.html"))
+    return FileResponse(os.path.join(CLIENT_DIR, "index.html"))
 
 @app.get("/admin", response_class=FileResponse)
 async def serve_admin():
-    return FileResponse(os.path.join("static", "admin.html"))
+    return FileResponse(os.path.join(CLIENT_DIR, "admin.html"))
